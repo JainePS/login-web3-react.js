28aa9072f36411dfeaea861d8b6bada982aa86db
Jaine Santos <jaine@valarisolutions.com>
2023-05-14 18:25:53 +0200

    feat: add styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Web3 from 'web3';
 
 function App() {
+const [isConnected, setIsConnected] = useState(false);
+  const detectCurrentProvider = () => {
+    let provider;
+    if (window.ethereum) {
+      provider = window.ethereum;
+      return provider;
+    }
+    if (window.web3) {
+      provider = window.web3.currentProvider;
+      return provider;
+    }
+    console.log('Non ethereum brownser detected, please install Metamask');
+    return provider;
+  }
+
+  const onConnect = async() => {
+    try {
+      const currentProvider = detectCurrentProvider();
+      if(currentProvider){
+        await currentProvider.request({method: 'eth_requestAccounts'});
+        const web3 = new Web3(currentProvider);
+        const userAccount = await web3.eth.getAccounts();
+        //If there is more than one account on the wallet, then it should bring the first one
+        const account = userAccount[0];
+      let ethBalance = await web3.eth.getBalance();
+      setIsConnected(true);
+      }
+    } catch (err) {
+      console.log(err);      
+    }
+  }
+const disconnect = () => {
+  setIsConnected(false);
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div className='app-header'></div>
+      <h1>React dApp authentication</h1>
+      <div className='app-wrapper'>
+{!isConnected && (
+  <button className='login-button' onClick={onConnect}>Login</button>
+)}
+      </div>
     </div>
   );
 }
